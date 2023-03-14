ccccea596e9577e8487e5e61f7b991cd23c6c265
Imtiyaz Akiwat <94955223+imtiyazakiwat@users.noreply.github.com>
2023-03-14 17:22:44 +0530

    removed wrong code  service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,4 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(function(registration) {
-        console.log('Service Worker registered with scope:', registration.scope);
-      }, function(err) {
-        console.log('Service Worker registration failed:', err);
-      });
-  });
-}
+
 const cacheName = 'my-website-cache-v1';
 const assetsToCache = [
   '/',
